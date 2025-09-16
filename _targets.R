db3c08edfa0e915eaa01cd075eb15ff6bbf2dbf1
XiaquZ <xiaqu.zhou@gmail.com>
2025-09-16 16:48:28 +0200

    Update models02.

--- _targets.R
+++ _targets.R
@@ -23,9 +23,9 @@ tar_option_set(
   resources = tar_resources(
     clustermq = tar_resources_clustermq(template = list(
       job_name = "Current-SDMs",
-      per_cpu_mem = "3470mb",
+      per_cpu_mem = "5100mb", #"3470mb",
       n_tasks = 1,
-      per_task_cpus = 72,
+      per_task_cpus = 36,
       walltime = "24:00:00"
     ))
   )

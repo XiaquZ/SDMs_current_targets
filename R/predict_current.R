78676b1021a2b97e7a648f705475e730af07a157
XiaquZ <xiaqu.zhou@gmail.com>
2025-09-17 10:24:22 +0200

    bigmem genius.

--- R/predict_current.R
+++ R/predict_current.R
@@ -15,7 +15,7 @@ predict_CurrentSDM <- function(input_folders, mdl_paths) {
   # tile_files <- tile_files[o]
   
   # Iterate through tiles (assumes tiles are numbered from 1 to 99)
-  for (i in 77:99) {
+  for (i in 78:99) {
     
     # Initialize an empty list to store predictors for this tile
     # Define predictor keywords
